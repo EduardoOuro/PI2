071a0c26c9ee3d8cf97505b6fe3cac3b74a30b2b
MARIA ISADORA MARBA BESERRA <maria.imbeserra@senacsp.edu.br>
2019-12-04 12:17:02 -0300

    View Cliente funcionando

---
+++ PI2/src/Dao/DaoCliente.java
@@ -0,0 +1,163 @@
+package Dao;
+
+import Connection.ConnectionFactory;
+import java.util.ArrayList;
+import Model.ModelCliente;
+
+public class DaoCliente extends ConnectionFactory {
+
+    /**
+     * Cadastrar um cliente no banco
+     *
+     * @param pModelCliente
+     * @return
+     */
+    public int salvarClienteDAO(ModelCliente pModelCliente) {
+        try {
+            this.conectar();
+            return this.insertSQL("INSERT INTO cliente("
+                    + "nome_cli,sobrenome_cli,cpf_cli, endereco_cli,bairro_cli,"
+                    + " cidade_cli,cep_cli,uf_cli,telefone_cli)"
+                    + "VALUES("
+                    + "'" + pModelCliente.getNomeCliente() + "',"
+                    + "'" + pModelCliente.getSobrenomeCliente() + "',"
+                    + "'" + pModelCliente.getCpfCliente() + "',"        
+                    + "'" + pModelCliente.getEnderecoCliente() + "',"
+                    + "'" + pModelCliente.getBairroCliente() + "',"
+                    + "'" + pModelCliente.getCidadeCliente() + "',"
+                    + "'" + pModelCliente.getCepCliente() + "',"        
+                    + "'" + pModelCliente.getUfCliente() + "',"
+                    + "'" + pModelCliente.getTelefoneCliente() + "'"
+                    + ");"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        } finally {
+            this.fecharConexao();
+        }
+    }
+
+    /**
+     * Excluir um cliente do banco
+     *
+     * @param pCodigoCliente
+     * @return boolean
+     */
+    public boolean excluirClienteDAO(int pCodigoCliente) {
+        try {
+            this.conectar();
+            return this.executarUpdateDeleteSQL("DELETE FROM cliente "
+                    + "WHERE cod_cli = " + pCodigoCliente);
+        } catch (Exception e) {
+            return false;
+
+        } finally {
+            this.fecharConexao();
+        }
+    }
+
+    /**
+     * Alterar dados do cliente
+     *
+     * @param pModelCliente
+     * @return boolean
+     */
+    public boolean alterarClienteDAO(ModelCliente pModelCliente) {
+        try {
+            this.conectar();
+            return this.executarUpdateDeleteSQL("UPDATE cliente SET "
+                    + "nome_cli = '" + pModelCliente.getNomeCliente() + "',"
+                    + "sobrenome_cli = '" + pModelCliente.getSobrenomeCliente() + "',"
+                    + "cpf_cli = '" + pModelCliente.getCpfCliente() + "',"        
+                    + "endereco_cli = '" + pModelCliente.getEnderecoCliente() + "',"
+                    + "bairro_cli = '" + pModelCliente.getBairroCliente() + "',"
+                    + "cidade_cli = '" + pModelCliente.getCidadeCliente() + "',"
+                    + "cep_cli = '" + pModelCliente.getCepCliente() + "',"
+                    + "uf_cli = '" + pModelCliente.getUfCliente() + "',"
+                    + "telefone_cli = '" + pModelCliente.getTelefoneCliente() + "'"
+                    + " WHERE cod_cli = '" + pModelCliente.getCodigoCliente() + "'");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            this.fecharConexao();
+        }
+
+    }
+
+    /**
+     * Retornar um cliente pelo código
+     *
+     * @param pCodigoCliente
+     * @return ModelCliente
+     */
+    public ModelCliente retornarClienteDAO(int pCodigoCliente) {
+        ModelCliente modelCliente = new ModelCliente();
+        try {
+            this.conectar();
+            this.executarSQL("SELECT "
+                    + "cod_cli, nome_cli,sobrenome_cli,cpf_cli,"
+                    + " endereco_cli,bairro_cli,"
+                    + " cidade_cli, cep_cli,uf_cli, telefone_cli"
+                    + " FROM cliente WHERE cod_cli = " + pCodigoCliente);
+            while (this.getResultSet().next()) {
+                modelCliente.setCodigoCliente(this.getResultSet().getInt("cod_cli"));
+                modelCliente.setNomeCliente(this.getResultSet().getString("nome_cli"));
+                modelCliente.setSobrenomeCliente(this.getResultSet().getString("sobrenome_cli"));
+                modelCliente.setCpfCliente(this.getResultSet().getString("cpf_cli"));
+                modelCliente.setEnderecoCliente(this.getResultSet().getString("endereco_cli"));
+                modelCliente.setBairroCliente(this.getResultSet().getString("bairro_cli"));
+                modelCliente.setCidadeCliente(this.getResultSet().getString("cidade_cli"));
+                modelCliente.setCepCliente(this.getResultSet().getString("cep_cli"));
+                modelCliente.setUfCliente(this.getResultSet().getString("uf_cli"));
+                modelCliente.setTelefoneCliente(this.getResultSet().getString("telefone_cli"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.fecharConexao();
+        }
+        return modelCliente;
+    }
+
+    /**
+     * Retornar uma lista completa de clientes
+     *
+     * @return listaModelCliente
+     */
+    public ArrayList<ModelCliente> retornarListaClienteDAO() {
+        ArrayList<ModelCliente> listaModelCliente = new ArrayList<>();
+        ModelCliente modelCliente = new ModelCliente();
+        try {
+            this.conectar();
+            this.executarSQL("SELECT "
+                    + "cod_cli, nome_cli,sobrenome_cli, cpf_cli,"
+                    + " endereco_cli,bairro_cli,"
+                    + "cidade_cli, cep_cli,uf_cli,telefone_cli "
+                    + "FROM cliente;");
+
+            while (this.getResultSet().next()) {
+                modelCliente.setCodigoCliente(this.getResultSet().getInt("cod_cli"));
+                modelCliente.setNomeCliente(this.getResultSet().getString("nome_cli"));
+                modelCliente.setSobrenomeCliente(this.getResultSet().getString("sobrenome_cli"));
+                modelCliente.setCpfCliente(this.getResultSet().getString("cpf_cli"));
+                modelCliente.setEnderecoCliente(this.getResultSet().getString("endereco_cli"));
+                modelCliente.setBairroCliente(this.getResultSet().getString("bairro_cli"));
+                modelCliente.setCidadeCliente(this.getResultSet().getString("cidade_cli"));
+                modelCliente.setCepCliente(this.getResultSet().getString("cep_cli"));
+                modelCliente.setUfCliente(this.getResultSet().getString("uf_cli"));
+                modelCliente.setTelefoneCliente(this.getResultSet().getString("telefone_cli"));
+               
+                listaModelCliente.add(modelCliente);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.fecharConexao();
+        }
+        return listaModelCliente;
+    }
+}
---
+++ PI2/src/Model/ModelCliente.java
@@ -0,0 +1,101 @@
+
+package Model;
+
+
+public class ModelCliente {
+
+    int codigoCliente;
+    String nomeCliente;
+    String sobrenomeCliente;
+    String cpfCliente;
+    String enderecoCliente;
+    String bairroCliente;
+    String cidadeCliente;
+    String cepCliente;
+    String ufCliente;
+
+    public String getSobrenomeCliente() {
+        return sobrenomeCliente;
+    }
+
+    public void setSobrenomeCliente(String sobrenomeCliente) {
+        this.sobrenomeCliente = sobrenomeCliente;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public void setCpfCliente(String cpfCliente) {
+        this.cpfCliente = cpfCliente;
+    }
+    String telefoneCliente;
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getEnderecoCliente() {
+        return enderecoCliente;
+    }
+
+    public void setEnderecoCliente(String enderecoCliente) {
+        this.enderecoCliente = enderecoCliente;
+    }
+
+    public String getBairroCliente() {
+        return bairroCliente;
+    }
+
+    public void setBairroCliente(String bairroCliente) {
+        this.bairroCliente = bairroCliente;
+    }
+
+    public String getCidadeCliente() {
+        return cidadeCliente;
+    }
+
+    public void setCidadeCliente(String cidadeCliente) {
+        this.cidadeCliente = cidadeCliente;
+    }
+
+    public String getUfCliente() {
+        return ufCliente;
+    }
+
+    public void setUfCliente(String ufCliente) {
+        this.ufCliente = ufCliente;
+    }
+
+    public String getTelefoneCliente() {
+        return telefoneCliente;
+    }
+
+    public void setTelefoneCliente(String telefoneCliente) {
+        this.telefoneCliente = telefoneCliente;
+    }
+
+    public String getCepCliente() {
+        return cepCliente;
+    }
+
+    public void setCepCliente(String cepCliente) {
+        this.cepCliente = cepCliente;
+    }
+    
+}
+
+    
+
